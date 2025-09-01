6fc0cd7d57a640d028d53ccf88c4c3407f0464e5
GabrielcitoHub <82218456+GabrielcitoHub@users.noreply.github.com>
2025-09-01 08:48:32 -0300

    added some util functions

--- init.lua
+++ init.lua
@@ -6,21 +6,50 @@ local Utils = {
 	cache        = {},
 }
 
-Utils.warn = function(...)
+function Utils:warn(...)
     print("Warning: " .. ...)
 end
+function Utils.warn(...)
+    Utils:warn(...)
+end
 
-Utils.getParentFolder = function(path)
+function warn(...)
+    Utils:warn(...)
+end
+
+function Utils:getParentFolder(path)
     return path:match("(.*/)[^/]+/?$") or ""
 end
 
-Utils.getRelativePath = function()
+function Utils:getRelativePath()
     -- Get the current file's path
     local info = debug.getinfo(1, "S").source
     local basePath = info:match("@(.*/)")
     basePath = basePath and basePath:gsub("\\", "/") or ""
     basePath = basePath:gsub("^./", "")  -- make relative to root
     return basePath
+end
+
+function Utils:setSearchPaths(paths, extensions)
+    Utils.paths = paths or {"assets/images/","assets/",""}
+    Utils.extensions = extensions or {".png", ""}
+end
+Utils:setSearchPaths()
+
+function Utils:findFile(path, paths, extensions)
+    if not path then return end
+    paths = Utils.paths or paths
+    extensions = Utils.extensions or extensions
+    for _,ext in ipairs(extensions) do
+        for _,testpath in ipairs(paths) do
+            local trypath = testpath .. path .. ext
+            local exists = love.filesystem.getInfo(trypath)
+            if exists then
+                local image = love.graphics.newImage(trypath)
+                return image
+            end
+        end
+    end
 end
 
 return Utils
